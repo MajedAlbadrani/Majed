708c32fc6b44a083b304a9835fe9d9c941941e3f
MajedAlbadrani <154915227+MajedAlbadrani@users.noreply.github.com>
2023-12-26 20:25:46 +0300

    Add files via upload

---
+++ majed.java
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>اسمك - موقع شخصي</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            padding: 20px;
+        }
+    </style>
+</head>
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        margin: 20px;
+        padding: 20px;
+        background-image: url('11.jpg'); /* 11.jpg' 11 */
+        background-size: cover;
+        background-position: center;
+        color: #ffffff; /* لتحسين قراءة النص على الخلفية */
+    }
+</style>
+<body>
+
+    <header>
+        <h1>مرحبًا بك في موقعي الشخصي</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="#">الصفحة الرئيسية</a></li>
+            <li><a href="#">من نحن</a></li>
+            <li><a href="#">اتصل بنا</a></li>
+        </ul>
+    </nav>
+
+    <section>
+        <h2>معلومات عني</h2>
+        <p>أنا [ماجد ]. أحب [الرياضه  ].</p>
+    </section>
+
+    <footer>
+        <p>جميع الحقوق محفوظة &copy; 2023</p>
+    </footer>
+<script>
+    function changeBackgroundColor() {
+        var colors = ['#3498db', '#e74c3c', '#2ecc71', '#f39c12', '#9b59b6'];
+        var randomColor = colors[Math.floor(Math.random() * colors.length)];
+        document.body.style.backgroundColor = randomColor;
+    }
+</script>
+
+</body>
+</html>
